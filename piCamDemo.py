a404ffd79825e8dbd7b9befd8e9ed76c15b30aca
Amit Jain <ajonnet@gmail.com>
2018-08-08 17:20:58 +0530

    ADDED: code to display continous frames from camera

--- piCamDemo.py
+++ piCamDemo.py
@@ -6,23 +6,21 @@ import pi3d
 from PIL import Image
 
 camera=PiCamera()
-
 rawCapture=PiRGBArray(camera)
 time.sleep(0.5)
-camera.capture(rawCapture, format="bgr")
-camera.close()
-cap_image=rawCapture.array
 
-cv2.imwrite("Image.jpg",cap_image)
-print("Image capture and written to disk")
+#camera.capture(rawCapture, format="bgr")
+#camera.close()
+#cap_image=rawCapture.array
+#cv2.imwrite("Image.jpg",cap_image)
+#print("Image capture and written to disk")
+captureImg()
 
-DISPLAY = pi3d.Display.create(x=150, y=150, frames_per_second=20)
+DISPLAY = pi3d.Display.create(x=10, y=150, frames_per_second=20)
 shader = pi3d.Shader("uv_flat")
 im = Image.open("Image.jpg")
 tex = pi3d.Texture(im)
 sprite = pi3d.ImageSprite(tex,shader,w=100.0,h=100.0,z=5.0)
-
-
 #sprite = pi3d.ImageSprite("textures/PATRN.PNG", shader, w=100.0, h=100.0, z=5.0)
 mykeys = pi3d.Keyboard()
 
@@ -30,8 +28,17 @@ while DISPLAY.loop_running():
 	sprite.draw()
 	sprite.position(100,100,5.0)
 
+	captureImg()
+	sprite = pi3d.ImageSprite("Image.jpg",shader,w=100.0,h=100.0,z=5.0)
+	
 	if mykeys.read() == 27:
 		mykeys.close()
 		DISPLAY.destroy()
+		camera.close()
 		break
-    
+
+
+def captureImg()
+	camera.capture(rawCapture, format="bgr")
+	cap_image = rawCapture.array
+	cv2.imwrite("Image.jpg",cap_image)
