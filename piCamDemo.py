15b45e172fc0e8f74bc3fbbcbfd1694b2cc58f7e
Amit Jain <ajonnet@gmail.com>
2018-08-08 18:44:14 +0530

    direct stream from camera

--- piCamDemo.py
+++ piCamDemo.py
@@ -5,8 +5,8 @@ import cv2
 import pi3d
 from PIL import Image
 
-cam_width = 128
-cam_height = 112
+cam_width = 320
+cam_height = 240
 
 camera = PiCamera()
 camera.resolution = (cam_width,cam_height)
@@ -27,7 +27,7 @@ time.sleep(1)
 #print("Image capture and written to disk")
 
 
-DISPLAY = pi3d.Display.create(x=10, y=150, frames_per_second=20)
+DISPLAY = pi3d.Display.create(x=10, y=10, frames_per_second=20)
 shader = pi3d.Shader("uv_flat")
 CAMERA = pi3d.Camera(is_3d=False)
 mykeys = pi3d.Keyboard()
@@ -40,7 +40,7 @@ mykeys = pi3d.Keyboard()
 
 while DISPLAY.loop_running():
 	rawCapture.truncate(0)
-	camera.capture(rawCapture, format="bgr", use_video_port=True)
+	camera.capture(rawCapture, format="rgb", use_video_port=True)
 	image = rawCapture.array;
 
 	#camera.capture("Image.jpg")
@@ -48,9 +48,9 @@ while DISPLAY.loop_running():
 	#cap_image = rawCapture.array
 	#cv2.imwrite("Image.jpg",cap_image)
 	
-	sprite = pi3d.ImageSprite(pi3d.Texture(image),shader,w=100.0,h=100.0,z=5.0)
+	sprite = pi3d.ImageSprite(pi3d.Texture(image),shader,w=cam_width,h=cam_height,z=5.0)
 	sprite.draw()
-	sprite.position(100,100,5.0)
+	sprite.position(10,10,5.0)
 	
 	if mykeys.read() == 27:
 		mykeys.close()
